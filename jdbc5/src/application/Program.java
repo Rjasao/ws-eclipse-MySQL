49edcd76f74f52f827ef79a663bae395be432c88
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-09-09 20:23:16 -0300

    MySQL - delete
    MySQL - delete

--- jdbc5/src/application/Program.java
+++ jdbc5/src/application/Program.java
@@ -9,7 +9,7 @@ import db.DbIntegrityException;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		
 		Connection conn = null;
 		PreparedStatement st = null;
