18087c4172653d60041c0da364d7489e61c05fc4
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-09-09 18:46:44 -0300

    MySQL-insere-v02
    insere dados com Java MySQL

--- jdbc3/src/application/Program.java
+++ jdbc3/src/application/Program.java
@@ -25,8 +25,7 @@ public class Program {
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)", 
-					Statement.RETURN_GENERATED_KEYS);
+					+ "(?, ?, ?, ?, ?)");
 
 			st.setString(1, "Carl Purple");
 			st.setString(2, "carl@gmail.com");
@@ -34,23 +33,8 @@ public class Program {
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
 
-			// EXAMPLE 2:
-			//st = conn.prepareStatement(
-			//		"insert into department (Name) values ('D1'),('D2')", 
-			//		Statement.RETURN_GENERATED_KEYS);
-
 			int rowsAffected = st.executeUpdate();
 			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id: " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected!");
-			}
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
