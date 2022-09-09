1645de59dcf5d43bf15387a036220aded6bd1dbb
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-09-09 20:54:09 -0300

    MySQL-Transacao final com commit
    Transacao com seguranca, so fara a transacao se todo processo nao possuir erro, caso ocorra voltara com os valores sem atualizacao- medida de seguranca.

--- jdbc6/src/application/Program.java
+++ jdbc6/src/application/Program.java
@@ -5,8 +5,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
-public class Program {
+public class Program { 
 
 	public static void main(String[] args) { 
 		
@@ -15,24 +16,32 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			
+			conn.setAutoCommit(false); // transacao com segurança
+			
 			st = conn.createStatement();
 			
-			int rows1 = st.executeUpdate("UPDATE seller BaseSalary = 2090 WHERE Departament = 1");
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 		
-			//erro provocada
-			int x =1;
-			if(x < 2) {
-				throw new SQLException("erro falso");
-				}
-			// =============
+			//erro provocada 
+			//int x =1;
+			//if(x < 2) {
+			//	throw new SQLException("erro falso");
+			//	}
+						
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
 			
-			int rows2 = st.executeUpdate("UPDATE seller BaseSalary = 3090 WHERE Departament = 2");
-	
+			conn.commit(); // fim da transacao com seguranca
+			
 			System.out.println("rows1 " + rows1);
 			System.out.println("rows2 " + rows2);
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
+			try {
+				conn.rollback();
+				throw new DbException("Transaction roller back! Cause by: " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Error trying to roller back! Cause by: " + e.getMessage());
+			}
 		}
 		finally {
 			DB.closeStatement(st);
