ba922d192876577ca5545f292cc2f48a89aa8ff2
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-09-11 01:20:07 -0300

    findById implementation
    SellerDaoJDBC implementou no FindById

--- demo-dao-jdbc/src/model/entities/Seller.java
+++ demo-dao-jdbc/src/model/entities/Seller.java
@@ -4,9 +4,9 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Seller implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	private String email;
